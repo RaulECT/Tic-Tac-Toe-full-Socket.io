f9275908e5a5b8673b3c115a4df1f9eef96ddcca
Raul <eduardoct120@gmail.com>
2016-10-14 14:25:08 -0500

    some changes

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.use("/js", express.static(__dirname + '/js'));
 
 server.listen(80, function(){
   console.log("Running on port 80");
-})
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("j");
+});
 
 var io = socket.listen(server);
 
